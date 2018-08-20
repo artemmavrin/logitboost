5d963aab2f42978c3b612fda56105135f436f1ed
Artem Mavrin <amavrin@ucsd.edu>
2018-08-19 22:40:53 -0700

    setup: added more trove classifiers

--- setup.py
+++ setup.py
@@ -27,10 +27,13 @@ setup(
     tests_require=["pytest"],
     zip_safe=False,
     classifiers=[
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.7"
     ]
 )

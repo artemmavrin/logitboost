ccda23170318cb499431e519fe67f47e76cb321e
Artem Mavrin <amavrin@ucsd.edu>
2018-09-16 23:04:29 -0700

    tests: updated test organization

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     url="https://github.com/artemmavrin/logitboost",
     author="Artem Mavrin",
     author_email="amavrin@ucsd.edu",
-    packages=sorted(find_packages()),
+    packages=sorted(find_packages(exclude=("*.tests",))),
     include_package_data=True,
     install_requires=[
         "numpy>=1.15",
